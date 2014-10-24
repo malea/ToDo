6f94920fddedfe3ca93763060804b032d7c14b6e
Malea Grubb <maleangrubb@gmail.com>
2014-10-24 19:21:36 -0400

    Create server.py

---
+++ server.py
@@ -0,0 +1,33 @@
+import random
+from flask import Flask, make_response
+
+app = Flask(__name__)
+    
+tasks = [
+    {
+        'id': 1, 
+        'userid': 1,
+        'content': u'Make a todo app',
+        'done': False
+    },
+    {
+        'id': 2,
+        'userid': 1,
+        'content': u'Buy groceries',
+        'done': False
+    },
+    {
+        'id': 3, 
+        'userid': 2,
+        'content': u'Add account functionality',
+        'done': False
+    }
+]
+
+@app.route('/')
+def index():
+    return make_response(open('index.html').read())
+
+
+if __name__ == '__main__':
+    app.run()
