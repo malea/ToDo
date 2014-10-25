6600b22dac517cadec3f890de3274b482abe56d6
Malea Grubb <maleangrubb@gmail.com>
2014-10-24 23:45:01 -0400

    Implement Flask API (just in mem, for now)

--- server.py
+++ server.py
@@ -1,25 +1,28 @@
 import random
-from flask import Flask, make_response
+import json
+import requests
+from flask import Flask, abort, make_response, request
 
 app = Flask(__name__)
     
+next_tid = 4
 tasks = [
     {
         'id': 1, 
-        'userid': 1,
-        'content': u'Make a todo app',
+        'userid': '118352708147713112407',
+        'text': u'Make a todo app',
         'done': False
     },
     {
         'id': 2,
-        'userid': 1,
-        'content': u'Buy groceries',
+        'userid': '118352708147713112407',
+        'text': u'Buy groceries',
         'done': False
     },
     {
         'id': 3, 
-        'userid': 2,
-        'content': u'Add account functionality',
+        'userid': '111111111111111',
+        'text': u'Add account functionality',
         'done': False
     }
 ]
@@ -28,5 +31,82 @@ tasks = [
 def index():
     return make_response(open('index.html').read())
 
+def getGoogleID():
+    google_idtoken = request.headers.get('Google-Id-Token')
+    google_response = requests.get('https://www.googleapis.com/oauth2/v1/tokeninfo',
+            params={'id_token': google_idtoken})
+    if google_response.status_code != 200:
+        abort(401)
+    parsed_token = google_response.json()
+    return parsed_token['user_id']
+
+@app.route('/api/tasks', methods=['GET', 'POST'])
+@app.route('/api/tasks/<taskid>', methods=['GET', 'PUT', 'DELETE'])
+def taskAPI(taskid=None):
+    if taskid is None and request.method == 'GET':
+        return listTasks()
+    elif request.method == 'GET':
+        return getTask(taskid)
+    elif request.method == 'POST':
+        return createTask()
+    elif request.method == 'PUT':
+        return updateTask(taskid)
+    elif request.method == 'DELETE':
+        return deleteTask(taskid)
+
+def listTasks():
+    google_id = getGoogleID()
+    results = [task for task in tasks if task['userid'] == google_id]
+    return json.dumps(results)
+
+def getTask(taskid):
+    """get a specific task (in json)"""
+    for task in tasks:
+        if task['id'] == taskid:
+            return json.dumps(task)
+    abort(404)
+
+
+def createTask():
+    """add a new task"""
+    # get task from request
+    task = request.get_json()['msg']
+
+    # give it a new id
+    global next_tid
+    new_id = next_tid
+    next_tid += 1
+    task['id'] = new_id
+
+    # add to our list
+    tasks.append(task)
+
+    # tell client it worked (200 OK)
+    return make_response('{}', 200)
+
+def updateTask(taskid):
+    """update existing task"""
+    found = False
+    for task in tasks:
+        if int(task['id']) == int(taskid):
+            task.update(request.get_json()['msg'])
+            found = True
+    if not found:
+        abort(404)
+    else:
+        return make_response('{}', 200)
+
+def deleteTask(taskid):
+    """delete existing task"""
+    task_tmp = None
+    for task in tasks:
+        if int(task['id']) == int(taskid):
+            task_tmp = task
+    if task_tmp is None:
+        abort(404)
+    else:
+        tasks.remove(task_tmp)
+        return make_response('{}', 204)
+
 if __name__ == '__main__':
-    app.run()
+    app.run(debug=True)
--- static/todo.js
+++ static/todo.js
@@ -15,11 +15,15 @@
                 // sign in succeeded
                 signinCtrl.loggedIn = true;
                 var idToken = authResult['id_token'];
+                $rootScope.fullToken = idToken;
                 $http.get('https://www.googleapis.com/oauth2/v1/tokeninfo',
                     {params: {id_token: idToken}})
                      .success(function(token) {
                         $rootScope.idToken = token;
                         signinCtrl.hasToken = true;
+
+                        // sorry :-(
+                        $rootScope.hacketyhack.refreshTodos();
                      })
                      .error(function() {
                          alert('Error getting login info from Google.');
@@ -41,22 +45,68 @@
             $rootScope.$apply();
         };
     }]);
-    app.controller('TodoController', ['$rootScope', function($rootScope) {
+    app.controller('TodoController', ['$http', '$rootScope',
+                   function($http, $rootScope) {
         var todoCtrl = this;
-        todoCtrl.todos = [];
         todoCtrl.search = {done: false};
+        todoCtrl.todos = [];
+
+        todoCtrl.refreshTodos = function() {
+            $http.get('/api/tasks', {headers: {
+                'Google-Id-Token': $rootScope.fullToken
+            }}).success(function(tasks) {
+                todoCtrl.todos = tasks;
+            })
+            .error(function() {
+                alert('Uh oh -- /api/tasks broke.');
+            });
+        };
+
+        // sorry :-(
+        $rootScope.hacketyhack = {}
+        $rootScope.hacketyhack.refreshTodos = todoCtrl.refreshTodos;
+
+        todoCtrl.updateTask = function(task) {
+            // PUT /api/tasks/:id
+            $http.put('/api/tasks/'+task.id, {
+                msg: task,
+                headers: {
+                    'Google-Id-Token': $rootScope.fullToken
+                }
+            })
+        }
 
         todoCtrl.addTodo = function() {
-            todoCtrl.todos.push({ text: todoCtrl.todoText,
-                done: false,
-                userid: $rootScope.idToken.user_id });
+            // POST /api/tasks
+            $http.post('/api/tasks', {
+                msg: {
+                    text: todoCtrl.todoText,
+                    done: false,
+                    userid: $rootScope.idToken.user_id
+                },
+                headers: {
+                    'Google-Id-Token': $rootScope.fullToken
+                }
+            })
+
+
             todoCtrl.todoText = '';
+            // now refresh to get the new ID for the task
+            // from server
+            todoCtrl.refreshTodos();
         };
         
         todoCtrl.deleteArchiveAll = function() {
             for (var i = todoCtrl.todos.length - 1; i >= 0; i--) {
+                var task = todoCtrl.todos[i];
                 if (todoCtrl.todos[i].done) {
                     todoCtrl.todos.splice(i,1);
+                    // DELETE /api/tasks/:id
+                    $http.delete('/api/tasks/'+task.id, {
+                        headers: {
+                            'Google-Id-Token': $rootScope.fullToken
+                        }
+                    })
                 }
             }
         }; 
@@ -74,6 +124,8 @@
         todoCtrl.markAllDone = function() {
             todoCtrl.todos.forEach( function(todo) {
                todo.done = true;
+               // also on server
+               todoCtrl.updateTask(todo);
             });
         }; 
     }]);
